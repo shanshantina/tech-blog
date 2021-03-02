73a79c01fc5badfec0a60c7dea1a04ef29226bb6
Shanshan Xu <tinaxu84@gmail.com>
2021-03-02 12:22:16 -0500

    create basic file folders, models, and GET routes for user

---
+++ models/index.js
@@ -0,0 +1,40 @@
+
+const Post = require('./Post');
+const User = require('./User');
+const Comment = require('./Comment');
+
+
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'   // question
+});
+
+
+module.exports = { Post, User, Comment };
