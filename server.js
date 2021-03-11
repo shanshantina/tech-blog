c3741b187ff772f98f64a3da32a3be48af615672
Shanshan Xu <tinaxu84@gmail.com>
2021-03-11 16:03:13 -0500

    hide the secret

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: "Super secret secret",
+    secret: process.env.SESS_SECRET,
     cookie: { maxAge:3600000 },
     rolling: true,
     resave: false,
